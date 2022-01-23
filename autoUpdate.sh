71ef318a6740dd80bff31aab0e5e5c7ed0f41939
Cameron Dugan <cameron.dugan@protonmail.com>
2022-01-23 17:40:55 -0500

    fixed print changes

--- autoUpdate.sh
+++ autoUpdate.sh
@@ -2,9 +2,7 @@
 	echo "Reloading from repo..."
 	git reset --hard origin > /dev/null
 	git pull > /dev/null
-	echo "Finish reload"
-	sleep 5
 	echo "Building website..."
 	bundle exec jekyll build
-	echo "Finish build"
+	echo "Done"
 )
