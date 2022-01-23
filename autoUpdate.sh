0f50359a32b2a5a15588a89fbcd597371974f924
Cameron Dugan <cameron.dugan@protonmail.com>
2022-01-23 17:39:50 -0500

    fixed autoUpdate.sh

--- autoUpdate.sh
+++ autoUpdate.sh
@@ -1,6 +1,10 @@
 (cd /home/pi/camerondugan.com 
-	git reset --hard origin
-	git pull
+	echo "Reloading from repo..."
+	git reset --hard origin > /dev/null
+	git pull > /dev/null
+	echo "Finish reload"
+	sleep 5
+	echo "Building website..."
 	bundle exec jekyll build
-	cp -r arbidor _site
+	echo "Finish build"
 )
