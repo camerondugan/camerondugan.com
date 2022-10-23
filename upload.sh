5c6e222dcfe69481ab5c776ac765c54d36ff0b85
Cameron Dugan <cameron.dugan@protonmail.com>
2022-10-23 08:34:52 -0400

    updated via script :)

--- upload.sh
+++ upload.sh
@@ -2,6 +2,9 @@ echo "Hiding Old Posts..."
 bash tagOld.sh
 echo "Building..."
 bundle exec jekyll build > /dev/null
+echo "Optimizing Assets..."
+jpegoptim assets/images/*/*.jpg
+optipng assets/images/*/*.png
 echo "Pushing to git..."
 git add . > /dev/null
 git commit -m "updated via script :)" > /dev/null
