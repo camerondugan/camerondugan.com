d71e648eabed8c8ed4e94975554437dfcd3d4f07
Cameron Dugan <cameron.dugan@protonmail.com>
2022-10-23 08:38:32 -0400

    updated via script :)

--- upload.sh
+++ upload.sh
@@ -4,11 +4,11 @@ echo "Building..."
 bundle exec jekyll build > /dev/null
 echo "Optimizing Assets..."
 jpegoptim assets/images/*/*.jpg > /dev/null #One Directory
-optipng assets/images/*/*.png > /dev/null 
+optipng assets/images/*/*.png -quiet > /dev/null 
 jpegoptim assets/images/*/*/*.jpg > /dev/null #Two Directories
-optipng assets/images/*/*/*.png > /dev/null
+optipng assets/images/*/*/*.png -quiet > /dev/null
 jpegoptim assets/images/*/*/*/*.jpg > /dev/null #Three Directories
-optipng assets/images/*/*/*/*.png > /dev/null #Too lazy to code properly
+optipng assets/images/*/*/*/*.png -quiet > /dev/null #Too lazy to code properly
 echo "Pushing to git..."
 git add . > /dev/null
 git commit -m "updated via script :)" > /dev/null
