9c3964e5e7be009c256508eeab221ed52e8ce525
Cameron Dugan <cameron.dugan@protonmail.com>
2024-09-24 22:28:21 -0400

    added devenv packages

--- devenv.nix
+++ devenv.nix
@@ -5,7 +5,11 @@
   env.GREET = "devenv";
 
   # https://devenv.sh/packages/
-  packages = [ pkgs.git ];
+  packages = with pkgs; [ 
+    git 
+    optipng
+    jpegoptim
+  ];
 
   # https://devenv.sh/languages/
   languages.ruby.enable = true;
